f999be39d0e3c7edb990f9c3c29edbeeb0e19c2d
Ruzimov Javlonbek Polvonboyevich <ruzimov_javlonbek_1997@mail.ru>
2021-08-06 08:12:49 +0500

    15_dars Uy sishi 1
    15_dars Uy sishi 1

---
+++ sariq_dev/darslar/15_dars_uy_ishi_1.py
@@ -0,0 +1,23 @@
+python_izohli_lugat ={
+  "Rasmiy_sayt":"-www.python.org",
+  "Pythonn":"-mashhur dasturlash tili.U Guido van Rossum tomonidan yaratilgan va 1991 -yilda chiqarilgan",
+  "Python":"-bu o'rganishga oson va shu bilan birga imkonyatlari yuqori bo'lgan oz sonlik zamonaviy dasturlash tillari qatoriga kiradi.Python yuqori darajadagi ma'lumotlar strukturasi va oddiy lekin samarador obyektga yo'naltirilgan dasturlash uslublarini taqdim etadi.",
+  "ishlatilish-sohasi":"-1).web dasturlash   2).Dasturiy taminot    3).Matematika      4).Tizim skriptlari",
+  "False":"-yolg'on.",
+  "True":"-rost.",
+  "None":"-bo'sh  obyekt.",
+  "with_as":"-konteks, menejeri.",
+  "break":"-sikldan chiqish.",
+  "class":"-metod va atributlarda iborat.",
+  "continue":"-sikldan keyingi iteratsiyaga o'tish.",
+  "from":"-moduldan bir nechta funksiyani immport qilish.",
+  "is":"-xotirani bitta joyida 2ta obyektni jo'natsa bo'ladimi",
+  "if":"-agar",
+  "else":"-aks holda",
+  "elif":"-aks holda agar.",
+  "for":"-for sikli"
+  }
+
+for kalit,qiymat in sorted(python_izohli_lugat.items()):
+  print(f"{kalit} {qiymat}  \n")
+input()
